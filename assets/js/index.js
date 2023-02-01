fa0aa4c47f4e2cf5ebc25ea05eb435574b5abd40
Angel Daniel RM <119967462+AngelRodriguez78@users.noreply.github.com>
2023-01-31 22:52:49 -0500

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,97 +1,105 @@
 console.log("Entro index.js");
 
-// let nombre = "Angel Daniel";
-
-// console.log(`Hola mi nombre es ${nombre}`);
-
-// let nombre2 = "Pedro";
-
-// console.log(`Hola mi nombre es ${nombre2}`);
-
-function saludarMensaje(name) {
-    let apellido = "Menchaca";
-    return `Hola mi nombre es ${name}, buenos dias`;
+let mascota = {
+    nombre: "Firulais",
+    edad: 4,
+    color: "Cafe",
+    raza: "Labrador",
+    peso: 8,
+    vacunas: ["Vacuna 1", "Vacuna 2"]
 }
 
-// console.log(saludarMensaje("Daniel"));
-// console.log(saludarMensaje("Pedro"));
+console.log(mascota.nombre);
+console.log(mascota.edad);
+console.log(mascota);
+console.log(mascota.vacunas[0]);
+console.log(mascota["vacunas"][1]);
+console.log(mascota.vacunas);
 
-let nombres = [
-    "Daniel",
-    "Pedro",
-    "Emmanuiel",
-    "Adriana",
-    "andres"
+const colors = [
+    { name: 'Red', code: '#ff0000' },
+    { name: 'Green', code: '#00ff00' },
+    { name: 'Blue', code: '#0000ff' },
+    { name: 'Yellow', code: '#ffff00' },
+    { name: 'Purple', code: '#800080' }
+]
+
+// console.log(colors[2]);
+
+colors[1].spanish ="verde"
+console.log(colors[1].spanish);
+console.log(colors);
+
+// quiero una funcion que reciba un arreglo(?) e imprima el arreglo, 
+// filtrando o buscando el color mandado//
+
+function filtrarColor(arreglo, color){
+    let arregloFiltrado = arreglo.find(item =>{
+       return item.name == color;
+    });
+
+    return arregloFiltrado;
+}
+
+console.log(filtrarColor(colors,"Blue"),"colores filtrados");
+
+
+let employeesWithSalary = [
+    {
+        name: "Jesus Cardenas",
+        age: 32,
+        position: "Manager",
+        salary: 500
+    },
+    {
+        name: "Manuel Martinez",
+        age: 27,
+        position: "Developer",
+        salary: 300
+    },
+    {
+        name: "Carlos Perez",
+        age: 27,
+        position: "Developer",
+        salary: 300
+    },
+    {
+        name: "Arturo Lopez",
+        age: 27,
+        position: "Developer",
+        salary: 300
+    },
+    {
+        name: "Juan Hernandez",
+        age: 27,
+        position: "Developer",
+        salary: 300
+    },
+    {
+        name: "Bob Johnson",
+        age: 35,
+        position: "Designer",
+        salary: 300
+    },
+    {
+        name: "Emily Davis",
+        age: 29,
+        position: "QA Engineer",
+        salary: 300
+    },
+    {
+        name: "William Brown",
+        age: 31,
+        position: "Product Manager",
+        salary: 300
+    }
 ];
 
-console.log(nombres);
-console.log(nombres[2]);
+function filtrarTrabajo(empleados){
+    let arregloFiltrado2 = empleados.filter(empleado => {
+        return empleado.position == "Developer"
+    });
+    return arregloFiltrado2;
+}
 
-let numbers = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5"
-];
-
-console.log(numbers);
-console.log(numbers[2]);
-
-// console.log(`La cantidad de elementos en nombres es: ${nombres.length}`);
-// console.log(`La cantidad de elementos en numeros es: ${numbers.length}`);
-
-let paises = ["Afghanistan",
-"Albania",
-"Algeria",
-"American Samoa",
-"Andorra",
-"Angola",
-"Austria",
-"Azerbaijan",
-"Bahamas (the)",
-"Bahrain",
-"Bangladesh",
-"Barbados",
-"Belarus",
-"Belgium",
-"Vanuatu",
-"Venezuela (Bolivarian Republic of)",
-"Viet Nam",
-"Virgin Islands (British)",
-"Virgin Islands (U.S.)",
-"Wallis and Futuna",
-"Western Sahara",
-"Yemen",
-"Zambia"];
-
-// console.warn(paises);
-
-// paises.push("Mexico");
-
-// console.log(paises);
-// paises.pop();
-// console.warn("Esto es un warning");
-// console.error("Esto es un error");
-// console.table(paises);
-
-
-
-//borrar con pop
-
-
-// paises.unshift("Canada");
-// paises.shift();
-// paises.splice(10,1);
-// paises.splice(10,5);
-// paises.splice(0,1, "test");
-// paises.splice(20,3);
-
-// console.table(paises);
-
-paises.push("Narnia");
-paises.sort().reverse;
-
-console.log(paises);
-
-
+console.log (filtrarTrabajo(employeesWithSalary))
